50c68a30074b1b9141b619cbbff0031d3be1e084
OswaldCardoz <145090621+OswaldCardoz@users.noreply.github.com>
2023-10-29 14:44:46 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 //Write your code here
+class API {
+    #secure;
+    url;
+    method;
+  
+    constructor(url, method = 'GET') {
+      this.url = url;
+      this.method = method;
+      this.#secure = url.startsWith('https');
+    }
+  
+    isSecure() {
+      return this.#secure;
+    }
+  
+    updateUrl(newUrl) {
+      this.url = newUrl;
+      this.#secure = newUrl.startsWith('https');
+    }
+  }
 
 
 
